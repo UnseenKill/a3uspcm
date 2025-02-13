8c2af7eda9b8c219fabfa9a1113240437dc5dc53
Oliver Schieche <post@oliver-schieche.de>
2025-02-13 11:36:26 +0100

    Make loadout available to all vehicle types

--- addons/loadout/CfgVehicles.hpp
+++ addons/loadout/CfgVehicles.hpp
@@ -1,15 +1,24 @@
-class CfgVehicles {
-    class LandVehicle;
+#define LOADOUT_ACTION_FOR_CLASS(className,parentClass) \
+    class className : parentClass {\
+        class ACE_Actions {\
+            class ACE_MainActions {\
+                class GVAR(LoadoutMenu) {\
+                    displayName = "$STR_A3USPCM_Loadout_MenuLoadout";\
+                    condition = QUOTE(alive _target);\
+                    insertChildren = QUOTE(call FUNC(getLoadoutMenuChildren));\
+                };\
+            };\
+        };\
+    }
 
-    class Car: LandVehicle {
-		class ACE_Actions {
-            class ACE_MainActions {
-                class GVAR(LoadoutMenu) {
-                    displayName = "$STR_A3USPCM_Loadout_MenuLoadout";
-                    condition = QUOTE(alive _target);
-                    insertChildren = QUOTE(call FUNC(getLoadoutMenuChildren));
-                };
-            };
-        };
-    };
+class CfgVehicles {
+    class Air;
+    class LandVehicle;
+    class Ship;
+
+    LOADOUT_ACTION_FOR_CLASS(Car, LandVehicle);
+    LOADOUT_ACTION_FOR_CLASS(Tank, LandVehicle);
+    LOADOUT_ACTION_FOR_CLASS(Helicopter, Air);
+    LOADOUT_ACTION_FOR_CLASS(Plane, Air);
+    LOADOUT_ACTION_FOR_CLASS(Ship_F, Ship);
 };
