e8ae155223533a8776a4a9c5c3648935025a9f53
Oliver Schieche <post@oliver-schieche.de>
2025-02-25 12:16:31 +0100

    Removed CBA boilerplate for debugging; added production hook instead

--- addons/fixes/script_component.hpp
+++ addons/fixes/script_component.hpp
@@ -1,16 +1,3 @@
 #define COMPONENT fixes
 #include "\z\a3uspcm\addons\main\script_mod.hpp"
-
-#ifdef DEBUG_ENABLED_FIXES
-    #define DEBUG_MODE_FULL
-#endif
-
-#ifdef DEBUG_SETTINGS_FIXES
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_FIXES
-#endif
-
-#ifndef DEBUG_MODE_FULL
-    #define DEBUG_MODE_FULL
-#endif
-
 #include "\z\a3uspcm\addons\main\script_macros.hpp"
--- addons/util/script_component.hpp
+++ addons/util/script_component.hpp
@@ -1,18 +1,5 @@
 #define COMPONENT util
 #include "\z\a3uspcm\addons\main\script_mod.hpp"
-
-#ifdef DEBUG_ENABLED_UTIL
-    #define DEBUG_MODE_FULL
-#endif
-
-#ifdef DEBUG_SETTINGS_UTIL
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_UTIL
-#endif
-
-#ifndef DEBUG_MODE_FULL
-    #define DEBUG_MODE_FULL
-#endif
-
 #include "\z\a3uspcm\addons\main\script_macros.hpp"
 
 // Don't attach objects, but delete them and load their class names into cargo
