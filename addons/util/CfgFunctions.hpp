542335c0736b77aa34d5e66946de7e9f206867d0
Oliver Schieche <post@oliver-schieche.de>
2025-03-15 19:08:36 +0100

    Fix voices

--- addons/util/CfgFunctions.hpp
+++ addons/util/CfgFunctions.hpp
@@ -10,6 +10,7 @@ class CfgFunctions {
             PATHTO_FNC(utilIsDaytime);
             PATHTO_FNC(utilOnA3UClientInitDone);
             PATHTO_FNC(utilPromptText);
+            PATHTO_FNC(utilSetUnitsSpeaker);
             PATHTO_FNC(utilSpreadPoint);
             PATHTO_FNC(utilUnlockArsenalItem);
             PATHTO_FNC(utilUnlockLoadout);
