b71335da8c61c1aeb17b1a432abb44fdc3468889
Oliver Schieche <post@oliver-schieche.de>
2025-03-15 19:38:56 +0100

    EMP to weapon dealer's store

--- addons/store/CfgHalsStore.hpp
+++ addons/store/CfgHalsStore.hpp
@@ -5,6 +5,8 @@ class CfgHALsAddons {
                 A3U_STORE_DEFITEM(ACE_painkillers,5,A3U_STORE_MISC_STOCK);
                 A3U_STORE_DEFITEM(ACE_suture,5,A3U_STORE_MISC_STOCK);
                 A3U_STORE_DEFITEM(EGVAR(despawnbeacon,PackedBeacon),750,A3U_STORE_MISC_STOCK);
+                A3U_STORE_DEFITEM(EGVAR(assets,40mm_EMP),75,A3U_STORE_MISC_STOCK);
+                A3U_STORE_DEFITEM(EGVAR(assets,Grenade_EMP),275,A3U_STORE_MISC_STOCK);
             };
         };
     };
