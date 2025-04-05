feb78b4914ddefcdb281e16ab4b5b1108f0d6ece
Oliver Schieche <post@oliver-schieche.de>
2025-04-05 21:02:00 +0200

    Reduce font-size of import/export editor, change font to monospace

--- addons/music/dialog.hpp
+++ addons/music/dialog.hpp
@@ -15,7 +15,7 @@ $[
 	[-1000,"StaticTitle",[1,"A3USPCM Playlist Editor",["0.215375 * safezoneW + safezoneX","0.181 * safezoneH + safezoneY","0.551719 * safezoneW","0.033 * safezoneH"],[-1,-1,-1,-1],[0,0.8,0,1],[-1,-1,-1,-1],"","-1"],[]],
 	[-1001,"StaticBackground",[1,"",["0.215313 * safezoneW + safezoneX","0.219 * safezoneH + safezoneY","0.5775 * safezoneW","0.561 * safezoneH"],[-1,-1,-1,-1],[0,0,0,0.8],[-1,-1,-1,-1],"","-1"],[]],
 	[-1602,"BtnClose",[1,"X",["0.77225 * safezoneW + safezoneX","0.181 * safezoneH + safezoneY","0.020625 * safezoneW","0.033 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
-	[-1400,"EditCopyPaste",[1,"",["0.226719 * safezoneW + safezoneX","0.236 * safezoneH + safezoneY","0.33 * safezoneW","0.528 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],["style = ST_MULTI;","fade = 1;"]],
+	[-1400,"EditCopyPaste",[1,"",["0.226719 * safezoneW + safezoneX","0.236 * safezoneH + safezoneY","0.33 * safezoneW","0.528 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],["style = ST_MULTI;","fade = 1;","font = |EtelkaMonospacePro|;","sizeEx = 0.025;"]],
 	[1500,"ListAllTracks",[1,"",["0.226719 * safezoneW + safezoneX","0.236 * safezoneH + safezoneY","0.33 * safezoneW","0.528 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],["type = CT_LISTNBOX;","idcLeft = -1;","idcRight = -1;","drawSideArrows = false;"]],
 	[1002,"StaticTreePosition",[1,"",["0.561875 * safezoneW + safezoneX","0.28 * safezoneH + safezoneY","0.221719 * safezoneW","0.44 * safezoneH"],[-1,-1,-1,-1],[0.4,0,0,1],[-1,-1,-1,-1],"","-1"],["deletable = 1;","onLoad = QUOTE(_this spawn FUNC(onInitPlaylistTree));","fade = 1;"]],
 	[1600,"BtnRemove",[1,"<<",["0.675312 * safezoneW + safezoneX","0.236 * safezoneH + safezoneY","0.108281 * safezoneW","0.033 * safezoneH"],[-1,-1,-1,-1],[0,0.8,0,1],[-1,-1,-1,-1],"Remove selection","-1"],[]],
@@ -67,6 +67,8 @@ class GVAR(dialog) {
 		{
 			style = ST_MULTI;
 			fade = 1;
+			font = "EtelkaMonospacePro";
+			sizeEx = 0.025;
 
 			idc = IDC_RSCA3USPCMTRACKLISTEDITORDIALOG_EDITCOPYPASTE;
 			x = QUOTE(0.226719 * safezoneW + safezoneX);
