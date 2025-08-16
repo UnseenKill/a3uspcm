3c519b2da923afe92ded1a2b401ce06e150aa36f
Oliver Schieche <post@oliver-schieche.de>
2025-08-16 16:06:03 +0200

    Supports leave a lot of ACE interaction menu errors when compat mode is on

--- addons/supports/script_component.hpp
+++ addons/supports/script_component.hpp
@@ -27,5 +27,6 @@
 ]
 
 #define A3UEUESRS_ADDON_CHECK_AND_BOUNCE() if isClass(configFile >> "CfgPatches" >> "a3ueunsrs_main") exitWith {\
+    FUNC(canUseSupportsMenu) = { false };\
     INFO("Addon a3ue-unseenkill-radio-supports detected. Skipping A3USPCM supports addon initialization."); \
 }
