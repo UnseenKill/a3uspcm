15f74431ab70798d1c355430fbfeff9f54527748
Oliver Schieche <post@oliver-schieche.de>
2025-11-14 12:27:29 +0100

    Revert "Return of the overzealous build system. Gotta trick its buggy type validation."
    This reverts commit 891ffb83d5d217d93bd0e904b93dda26632ae724.

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -7,5 +7,5 @@
 #define ASSUME_VAR_TYPE(NAME,VALUE) (if false then { NAME = VALUE })
 #define INIT_SAVE_GVAR(NAME) if true then {\
     GVAR(NAME) = false;\
-    ASSUME_VAR_TYPE(GVAR(NAME),createHashMap);\
+    if false then { GVAR(NAME) = createHashMap };\
 }
