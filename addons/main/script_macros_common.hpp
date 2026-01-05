d4b8ab61bcf1a6d8df88453bf00742dca56cf376
Oliver Schieche <post@oliver-schieche.de>
2026-01-05 20:31:41 +0100

    PFORMAT patches no longer needed

--- addons/main/script_macros_common.hpp
+++ addons/main/script_macros_common.hpp
@@ -1,24 +1,6 @@
 #define MAINPREFIX z
 #include "\x\cba\addons\main\script_macros_common.hpp"
 
-// HEMTT complains about padded arguments; the original macro has a newline in it
-#undef PFORMAT_2
-#undef PFORMAT_3
-#undef PFORMAT_4
-#undef PFORMAT_5
-#undef PFORMAT_6
-#undef PFORMAT_7
-#undef PFORMAT_8
-#undef PFORMAT_9
-#define PFORMAT_2(MESSAGE,A,B) format ['%1: A=%2, B=%3', MESSAGE, RETNIL(A), RETNIL(B)]
-#define PFORMAT_3(MESSAGE,A,B,C) format ['%1: A=%2, B=%3, C=%4', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C)]
-#define PFORMAT_4(MESSAGE,A,B,C,D) format ['%1: A=%2, B=%3, C=%4, D=%5', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D)]
-#define PFORMAT_5(MESSAGE,A,B,C,D,E) format ['%1: A=%2, B=%3, C=%4, D=%5, E=%6', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E)]
-#define PFORMAT_6(MESSAGE,A,B,C,D,E,F) format ['%1: A=%2, B=%3, C=%4, D=%5, E=%6, F=%7', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F)]
-#define PFORMAT_7(MESSAGE,A,B,C,D,E,F,G) format ['%1: A=%2, B=%3, C=%4, D=%5, E=%6, F=%7, G=%8', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F), RETNIL(G)]
-#define PFORMAT_8(MESSAGE,A,B,C,D,E,F,G,H) format ['%1: A=%2, B=%3, C=%4, D=%5, E=%6, F=%7, G=%8, H=%9', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F), RETNIL(G), RETNIL(H)]
-#define PFORMAT_9(MESSAGE,A,B,C,D,E,F,G,H,I) format ['%1: A=%2, B=%3, C=%4, D=%5, E=%6, F=%7, G=%8, H=%9, I=%10', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F), RETNIL(G), RETNIL(H), RETNIL(I)]
-
 #undef PREP
 #undef PREPMAIN
 #ifdef DISABLE_COMPILE_CACHE
