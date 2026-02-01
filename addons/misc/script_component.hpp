848d9d071cedad64abb3fcc8ff31342ffa985a32
Oliver Schieche <post@oliver-schieche.de>
2026-02-01 16:01:37 +0100

    Hook into A3U flag action, add reveal action for uncaptured locations

--- addons/misc/script_component.hpp
+++ addons/misc/script_component.hpp
@@ -1,3 +1,5 @@
 #define COMPONENT misc
 #include "\z\a3uspcm\addons\main\script_mod.hpp"
 #include "\z\a3uspcm\addons\main\script_macros.hpp"
+
+#define CBA_EVENT_A3U_FLAGACTION QUOTE(TRIPLES(ADDON,event,A3U_flagAction))
