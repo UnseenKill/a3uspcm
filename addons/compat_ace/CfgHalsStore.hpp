ad2b4911fcf3ab3357c5b9776a4ae98b818300f6
Oliver Schieche <post@oliver-schieche.de>
2026-02-06 19:32:28 +0100

    Stores cleanup. Created A3USPCM store w/ own items, RHSUSF store if addon loaded

--- addons/compat_ace/CfgHalsStore.hpp
+++ addons/compat_ace/CfgHalsStore.hpp
@@ -24,6 +24,8 @@ class CfgHALsAddons {
                 displayName = __EVAL(formatText ["%1 %2", "ACE3", localize "STR_A3AU_misc"]);
     			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\backpack_ca.paa";
 
+                ITEM(ACE_painkillers,5,PN_STOCK)
+                ITEM(ACE_suture,5,PN_STOCK)
                 ITEM(ACE_DBAL_A3_Green,250,PN_STOCK)
                 ITEM(ACE_DBAL_A3_Red,250,PN_STOCK)
                 ITEM(ACE_Fortify,250,PN_STOCK)
